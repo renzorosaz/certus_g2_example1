009c901278eb525780772f3bb92bb606678995db
Renzo Martin Rosas Vidal <renzo.rosas@servexternos.grupoavalon.es>
2022-12-14 18:35:19 -0500

    preferencia de usuario

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../main_drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? fullname;
+
+  @override
+  void initState() {
+    validarPreferenciasUser();
+    super.initState();
+  }
+
+  Future<void> validarPreferenciasUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    fullname = prefs.getString('fullname');
+    print(fullname);
+  }
+  //TAREA SHARED PREFERENCES: public
+
+  // MOSTRAR EL FULLNAME EN MAINDRAWER
 
   @override
   Widget build(BuildContext context) {
